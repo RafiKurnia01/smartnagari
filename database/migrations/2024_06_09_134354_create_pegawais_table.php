f3e96f01b9b94edeb6f1dff43a6a6e5b681023ae
RafiKurnia01 <rafikurniailahi00@gmail.com>
2024-06-09 21:58:25 +0700

    step1

---
+++ database/migrations/2024_06_09_134354_create_pegawais_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIP');
+            $table->string('namapegawai');
+            $table->string('alamat');
+            $table->date('tgllahir');
+            $table->foreignId('jabatan_id')->constrained('jabatans')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
